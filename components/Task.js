111acb080a5b7d89e2e7560749d88a54f3d7247c
pasztor-andras <andras.pasztor1@gmail.com>
2022-02-17 22:41:58 +0100

    add basic components

---
+++ components/Task.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const Task = () => {
+  return (
+    <View>
+      <Text>This is task</Text>
+    </View>
+  );
+};
+
+const style = StyleSheet.create({
+    
+});
+
+
+export default Task;
