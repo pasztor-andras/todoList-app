5b69e35dee4b61db0ce4a2b25cb5026eb8386378
pasztor-andras <andras.pasztor1@gmail.com>
2022-02-18 12:07:22 +0100

    add styles and state base

--- App.js
+++ App.js
@@ -1,17 +1,33 @@
-import { StyleSheet, Text, View } from "react-native";
-import  Task from "./components/Task";
+import React, { useState } from "react";
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, View, TouchableOpacity } from "react-native";
+import Task from "./components/Task";
 
 export default function App() {
+
+  const [task, setTask] = useState();
+
+  const handleAddTask = () => {
+    
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.tasksWrapper}>
         <Text style={styles.sectionTitle}>Today's tasks</Text>
         <View style={styles.items}>
           {/* This is where the tasks will go! */}
-          <Task text={"Task 1"}/>
-          <Task text={"Task 2"}/>
+          <Task text={"Task 1"} />
+          <Task text={"Task 2"} />
         </View>
       </View>
+      <KeyboardAvoidingView behavior={Platform.OS === "android" ? "padding" : "height"} style={styles.writeTaskWrapper}>
+        <TextInput style={styles.input} placeholder={"Write a task"} value={task} onChangeText={text => setTask(text)} />
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -32,4 +48,33 @@ const styles = StyleSheet.create({
   items: {
     marginTop: 30,
   },
+  writeTaskWrapper: {
+    position: 'absolute',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    flexDirection: 'row',
+    bottom: 60,
+    width: '100%',
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    width: 250,
+    borderRadius: 60,
+    borderColor: "rgb(220, 220, 220)",
+    borderWidth: 1,
+    backgroundColor: "rgb(255, 255, 255)",
+  },
+  addWrapper: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 60,
+    height: 60,
+    backgroundColor: "rgb(255, 255, 255)",
+    borderColor: "rgb(220, 220, 220)",
+    borderWidth: 1,
+    borderRadius: 60,
+
+  },
+  addText: {},
 });
